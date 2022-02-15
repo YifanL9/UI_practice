b28031912a50e29bc12663d8b305113de5993a6a
yifanl <53729618+YifanL9@users.noreply.github.com>
2022-02-15 02:38:40 -0600

    update

---
+++ shop/script.js
@@ -0,0 +1,18 @@
+let isSubscribed = true;
+
+function addFamilyMember() {
+  alert("Please contact us to add a new member in your Family Plan!");
+  contact = document.getElementById("contact");
+  contact.scrollIntoView();
+}
+
+function logout() {
+  alert(
+    "You have been logged out of the system. Please close out of all tabs."
+  );
+}
+
+function toggleMembershipSubscription() {
+  isSubscribed = !isSubscribed;
+  document.getElementById("membershipSubscription").innerHTML = isSubscribed;
+}
